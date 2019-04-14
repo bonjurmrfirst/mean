d327b625e8c443347b8b6a5dfe633a4c266261e3
Illia Potsepaiev <illia_potsepaiev@epam.com>
2019-04-14 15:51:20 +0300

    3

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -4,8 +4,11 @@
  **/
 
 import * as express from 'express';
+import * as morgan from 'morgan';
 
 const app = express();
+
+app.use(morgan('combined'));
 
 app.get('/api', (req, res) => {
   res.send({message: `Welcome to api!`});
